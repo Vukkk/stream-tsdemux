e50c0e3aec784a9f7dbe204bd1115d7606aa4049
Vukkk <kleisberu@protonmail.com>
2021-07-16 15:08:54 +0200

    extended example with id3 stream

--- example.js
+++ example.js
@@ -4,9 +4,11 @@ const { TSDemuxer } = require('./index.js');
 const source = fs.createReadStream('./samples/av.ts');
 const video = fs.createWriteStream('./output-video.h264');
 const audio = fs.createWriteStream('./output-audio.aac');
+const id3 = fs.createWriteStream('./output-id3.bin');
 
 const demuxer = new TSDemuxer({
   video: video,
   audio: audio,
+  id3: id3,
 });
 source.pipe(demuxer);
